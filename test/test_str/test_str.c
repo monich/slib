287ae83152f974a049aa332b3e94c25aaa850390
Slava Monich <slava@monich.com>
2020-01-19 21:10:21 +0200

    Start using TEST_ASSERT

--- test/test_str/test_str.c
+++ test/test_str/test_str.c
@@ -1,7 +1,7 @@
 /*
- * $Id: test_str.c,v 1.3 2016/10/03 09:15:05 slava Exp $
+ * $Id: test_str.c,v 1.4 2020/01/19 19:10:07 slava Exp $
  *
- * Copyright (C) 2016 by Slava Monich
+ * Copyright (C) 2016-2020 by Slava Monich
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,7 +39,6 @@ TestStatus
 test_str_alloc(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     StrBuf* sb;
     Vector v;
     char* s;
@@ -55,122 +54,88 @@ test_str_alloc(
         "0123456789012345678901234567890123456789012345678";
 
     /* Test NULL resistance */
-    if (STRING_ToUnicode(NULL) ||
-        STRING_ToMultiByte(NULL) ||
-        STRING_ToMultiByteN(NULL, 0) ||
-        STRING_ToUnicode(NULL) ||
-        STRING_ToUnicodeN(NULL, 0)) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_ToUnicode(NULL));
+    TEST_ASSERT(!STRING_ToMultiByte(NULL));
+    TEST_ASSERT(!STRING_ToMultiByteN(NULL, 0));
+    TEST_ASSERT(!STRING_ToUnicode(NULL));
+    TEST_ASSERT(!STRING_ToUnicodeN(NULL, 0));
 
     /* Simulate allocation failures */
     testMem.failAt = testMem.allocCount;
-    if (STRING_Dup8("test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_Dup8("test"));
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_DupU(L"test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_DupU(L"test"));
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_Format(NULL, 0, "%s", "test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_Format(NULL, 0, "%s", "test"));
 
     testMem.failAt = testMem.allocCount;
     s = STRING_Format(buf, COUNT(buf), "%s", "test");
-    if (s != buf || strcmp(s, "t")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s == buf && !strcmp(s, "t"));
 
     testMem.failAt = testMem.allocCount;
     buf[0] = 'x';
     s = STRING_Format(buf, 0, "%s", "test");
-    if (s != buf || buf[0] != 'x') {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s == buf && buf[0] == 'x');
 
     testMem.failAt = testMem.allocCount + 1;
     s = STRING_Format(NULL, 0, "%s", longstr);
-    if (!s || strcmp(s, longstr99)) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s && !strcmp(s, longstr99));
     MEM_Free(s);
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_ToUnicode("test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_ToUnicode("test"));
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_ToUnicode("\xD1\x82\xD0\xB5\xD1\x81\xD1\x82")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_ToUnicode("\xD1\x82\xD0\xB5\xD1\x81\xD1\x82"));
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_ToMultiByte(L"test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_ToMultiByte(L"test"));
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_ToMultiByte(wtest)) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_ToMultiByte(wtest));
 
     sb = STRBUF_Create();
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_FormatFloat(sb, 0.1f)) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_FormatFloat(sb, 0.1f));
 
     testMem.failAt = testMem.allocCount;
-    if (STRING_FormatDouble(sb, 0.1)) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!STRING_FormatDouble(sb, 0.1));
 
     VECTOR_Init(&v, 0, vectorEqualsString, vectorFreeValueProc);
 
     /* Element has to be long enough not to fit into the buffer on stack */
     testMem.failAt = testMem.allocCount;
-    if (STRING_Split("0123456789012345678901234567890123456789,2",
-        NULL, ",", False) >= 0) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(STRING_Split("0123456789012345678901234567890123456789,2",
+        NULL, ",", False) <= 0);
 
-    for (i=0; i<4; i++) {
+    for (i = 0; i < 4; i++) {
         testMem.failAt = testMem.allocCount + i;
         VECTOR_Trim(&v);
-        if (STRING_Split(",", &v, ",", True) >= 0 ||
-            VECTOR_Size(&v) > 0) {
-            ret = TEST_ERR;
-        }
+        TEST_ASSERT(STRING_Split(",", &v, ",", True) < 0);
+        TEST_ASSERT(!VECTOR_Size(&v));
     }
 
-    for (i=0; i<4; i++) {
+    for (i = 0; i < 4; i++) {
         testMem.failAt = testMem.allocCount + i;
         VECTOR_Trim(&v);
-        if (STRING_Split("1,2", &v, ",", False) >= 0 ||
-            VECTOR_Size(&v) > 0) {
-            ret = TEST_ERR;
-        }
+        TEST_ASSERT(STRING_Split("1,2", &v, ",", False) < 0);
+        TEST_ASSERT(!VECTOR_Size(&v));
     }
 
-    for (i=0; i<7; i++) {
+    for (i = 0; i < 7; i++) {
         testMem.failAt = testMem.allocCount + i;
         VECTOR_Trim(&v);
-        if (STRING_Split(",1,2,", &v, ",", True) >= 0 ||
-            VECTOR_Size(&v) > 0) {
-            ret = TEST_ERR;
-        }
+        TEST_ASSERT(STRING_Split(",1,2,", &v, ",", True) < 0);
+        TEST_ASSERT(!VECTOR_Size(&v));
     }
 
     testMem.failAt = -1;
     STRBUF_Delete(sb);
     VECTOR_Destroy(&v);
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -178,7 +143,6 @@ TestStatus
 test_str_indexof(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     static const struct _test_str_indexof {
         Str s;
         Char c;
@@ -192,13 +156,15 @@ test_str_indexof(
         { TEXT("test"), 'e', 1, 1 }
     };
     int i;
-    for (i=0; i<COUNT(tests); i++) {
-        if (STRING_IndexOf(tests[i].s, tests[i].c) != tests[i].first ||
-            STRING_LastIndexOf(tests[i].s, tests[i].c) != tests[i].last) {
-            ret = TEST_ERR;
-        }
+
+    for (i = 0; i < COUNT(tests); i++) {
+        Str s = tests[i].s;
+        Char c = tests[i].c;
+
+        TEST_ASSERT(STRING_IndexOf(s, c) == tests[i].first);
+        TEST_ASSERT(STRING_LastIndexOf(s, c) == tests[i].last);
     }
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -206,7 +172,6 @@ TestStatus
 test_str_hash(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     static const struct _test_str_hash {
         Str s;
         int hash;
@@ -217,13 +182,12 @@ test_str_hash(
         { TEXT("Test"), 2603186, 2571410 }
     };
     int i;
-    for (i=0; i<COUNT(tests); i++) {
-        if (STRING_HashCode(tests[i].s) != tests[i].hash ||
-            STRING_HashCodeNoCase(tests[i].s) != tests[i].hashNoCase) {
-            ret = TEST_ERR;
-        }
+
+    for (i = 0; i < COUNT(tests); i++) {
+        TEST_ASSERT(STRING_HashCode(tests[i].s) == tests[i].hash);
+        TEST_ASSERT(STRING_HashCodeNoCase(tests[i].s) == tests[i].hashNoCase);
     }
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -231,28 +195,23 @@ TestStatus
 test_str_dup(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     char* s;
     wchar_t* ws;
 
-    if (STRING_Dup(NULL) ||
-        STRING_DupU(NULL)) {
-        ret = TEST_ERR;
-    }
+    ASSERT(!STRING_Dup(NULL));
+    ASSERT(!STRING_DupU(NULL));
 
     s = STRING_Dup8("test");
-    if (strcmp(s, "test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s);
+    TEST_ASSERT(!strcmp(s, "test"));
 
     ws = STRING_DupU(L"test");
-    if (wcscmp(ws, L"test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(ws);
+    TEST_ASSERT(!wcscmp(ws, L"test"));
 
     MEM_Free(s);
     MEM_Free(ws);
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -260,7 +219,6 @@ TestStatus
 test_str_startswith(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     static const struct _test_str_startswith {
         const char* s;
         const wchar_t* ws;
@@ -281,19 +239,19 @@ test_str_startswith(
         { S_("Test"), S_("t"), False, True }
     };
     int i;
-    for (i=0; i<COUNT(tests); i++) {
+
+    for (i = 0; i < COUNT(tests); i++) {
         const char* s = tests[i].s;
         const char* p = tests[i].p;
         const wchar_t* ws = tests[i].ws;
         const wchar_t* wp = tests[i].wp;
-        if (STRING_StartsWith8(s, p) != tests[i].starts ||
-            STRING_StartsWithNoCase8(s, p) != tests[i].startsNoCase ||
-            STRING_StartsWithU(ws, wp) != tests[i].starts ||
-            STRING_StartsWithNoCaseU(ws, wp) != tests[i].startsNoCase) {
-            ret = TEST_ERR;
-        }
+
+        TEST_ASSERT(STRING_StartsWith8(s, p) == tests[i].starts);
+        TEST_ASSERT(STRING_StartsWithNoCase8(s, p) == tests[i].startsNoCase);
+        TEST_ASSERT(STRING_StartsWithU(ws, wp) == tests[i].starts);
+        TEST_ASSERT(STRING_StartsWithNoCaseU(ws, wp) == tests[i].startsNoCase);
     }
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -301,7 +259,6 @@ TestStatus
 test_str_endswith(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     static const struct _test_str_endswith {
         const char* s;
         const wchar_t* ws;
@@ -320,20 +277,20 @@ test_str_endswith(
         { S_("test"), S_("T"), False, True },
         { S_("tesT"), S_("t"), False, True }
     };
+
     int i;
-    for (i=0; i<COUNT(tests); i++) {
+    for (i = 0; i < COUNT(tests); i++) {
         const char* s = tests[i].s;
         const char* suffix = tests[i].suffix;
         const wchar_t* ws = tests[i].ws;
         const wchar_t* wsuffix = tests[i].wsuffix;
-        if (STRING_EndsWith8(s, suffix) != tests[i].ends ||
-            STRING_EndsWithNoCase8(s, suffix) != tests[i].endsNoCase ||
-            STRING_EndsWithU(ws, wsuffix) != tests[i].ends ||
-            STRING_EndsWithNoCaseU(ws, wsuffix) != tests[i].endsNoCase) {
-            ret = TEST_ERR;
-        }
+
+        TEST_ASSERT(STRING_EndsWith8(s, suffix) == tests[i].ends);
+        TEST_ASSERT(STRING_EndsWithNoCase8(s, suffix) == tests[i].endsNoCase);
+        TEST_ASSERT(STRING_EndsWithU(ws, wsuffix) == tests[i].ends);
+        TEST_ASSERT(STRING_EndsWithNoCaseU(ws, wsuffix) == tests[i].endsNoCase);
     }
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -341,45 +298,37 @@ TestStatus
 test_str_unicode(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     static const wchar_t wtest[] = {0x0442, 0x0435, 0x0441, 0x0442, 0};
     wchar_t* ws;
     char* s;
 
     ws = STRING_ToUnicode("test");
-    if (wcscmp(ws, L"test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(ws);
+    TEST_ASSERT(!wcscmp(ws, L"test"));
     MEM_Free(ws);
 
     s = STRING_ToMultiByte(L"test");
-    if (!s || strcmp(s, "test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s);
+    TEST_ASSERT(!strcmp(s, "test"));
     MEM_Free(s);
 
     /* The rest may (and often does) fail on Windows */
 #ifndef _WIN32
     ws = STRING_ToUnicode("\xD1\x82\xD0\xB5\xD1\x81\xD1\x82");
-    if (wcscmp(ws, wtest)) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(ws);
+    TEST_ASSERT(!wcscmp(ws, wtest));
     MEM_Free(ws);
 
     /* Invalid UTF-8 sequence */
     ws = STRING_ToUnicode("\xD1\x82\xD0\xB5\xD1\x81\xD1");
-    if (ws) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!ws);
 
     s = STRING_ToMultiByte(wtest);
-    if (!s || strcmp(s, "\xD1\x82\xD0\xB5\xD1\x81\xD1\x82")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s);
+    TEST_ASSERT(!strcmp(s, "\xD1\x82\xD0\xB5\xD1\x81\xD1\x82"));
     MEM_Free(s);
 #endif
-
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -387,7 +336,6 @@ TestStatus
 test_str_format(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     char buf2[2];
     char* s;
     StrBuf* sb;
@@ -397,60 +345,30 @@ test_str_format(
         "01234567890123456789012345678901234567890123456789";
 
     s = STRING_Format(buf2, COUNT(buf2), "%s", "test");
-    if (s == buf2 || !s || strcmp(s, "test")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s != buf2);
+    TEST_ASSERT(s);
+    TEST_ASSERT(!strcmp(s, "test"));
     MEM_Free(s);
 
     s = STRING_Format(NULL, 0, "%s", longstr);
-    if (!s || strcmp(s, longstr)) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(s);
+    TEST_ASSERT(!strcmp(s, longstr));
     MEM_Free(s);
 
     sb = STRBUF_Create();
-    if (strcmp(STRING_FormatDouble(sb, 0.), "0.0")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatDouble(sb, 0.1), "0.1")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatDouble(sb, -0.1), "-0.1")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatDouble(sb, 3.14159265358979),"3.14159265358979")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatFloat(sb, 0.f), "0.0")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatFloat(sb, 0.1f), "0.1")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatFloat(sb, -0.1f), "-0.1")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatFloat(sb, 0.111f), "0.111")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatFloat(sb, 0.111f), "0.111")) {
-        ret = TEST_ERR;
-    }
-
-    if (strcmp(STRING_FormatFloat(sb, 3.141592f), "3.141592")) {
-        ret = TEST_ERR;
-    }
+    TEST_ASSERT(!strcmp(STRING_FormatDouble(sb, 0.), "0.0"));
+    TEST_ASSERT(!strcmp(STRING_FormatDouble(sb, 0.1), "0.1"));
+    TEST_ASSERT(!strcmp(STRING_FormatDouble(sb, -0.1), "-0.1"));
+    TEST_ASSERT(!strcmp(STRING_FormatDouble(sb, 3.14159265358979),
+        "3.14159265358979"));
+    TEST_ASSERT(!strcmp(STRING_FormatFloat(sb, 0.f), "0.0"));
+    TEST_ASSERT(!strcmp(STRING_FormatFloat(sb, 0.1f), "0.1"));
+    TEST_ASSERT(!strcmp(STRING_FormatFloat(sb, -0.1f), "-0.1"));
+    TEST_ASSERT(!strcmp(STRING_FormatFloat(sb, 0.111f), "0.111"));
+    TEST_ASSERT(!strcmp(STRING_FormatFloat(sb, 3.141592f), "3.141592"));
 
     STRBUF_Delete(sb);
-    return ret;
+    return TEST_OK;
 }
 
 static
@@ -458,10 +376,8 @@ TestStatus
 test_str_split(
     const TestDesc* test)
 {
-    TestStatus ret = TEST_OK;
     Vector v;
     int i;
-
     static const struct _test_str_split {
         Str s;
         Str delim;
@@ -483,25 +399,22 @@ test_str_split(
     };
 
     VECTOR_Init(&v, 0, vectorEqualsString, vectorFreeValueProc);
-    for (i=0; i<COUNT(tests); i++) {
-        Str s = tests[i].s, delim = tests[i].delim;
-        if (STRING_Split(s, NULL, delim, True) != tests[i].countEmpty ||
-            STRING_Split(s, NULL, delim, False) != tests[i].countNonEmpty) {
-            ret = TEST_ERR;
-        }
+    for (i = 0; i < COUNT(tests); i++) {
+        Str s = tests[i].s, d = tests[i].delim;
+
+        TEST_ASSERT(STRING_Split(s, NULL, d, True) == tests[i].countEmpty);
+        TEST_ASSERT(STRING_Split(s, NULL, d, False) == tests[i].countNonEmpty);
+
         VECTOR_Clear(&v);
-        if (STRING_Split(s, &v, delim, True) != tests[i].countEmpty ||
-            VECTOR_Size(&v) != tests[i].countEmpty) {
-            ret = TEST_ERR;
-        }
+        TEST_ASSERT(STRING_Split(s, &v, d, True) == tests[i].countEmpty);
+        TEST_ASSERT(VECTOR_Size(&v) == tests[i].countEmpty);
+
         VECTOR_Clear(&v);
-        if (STRING_Split(s, &v, delim, False) != tests[i].countNonEmpty ||
-            VECTOR_Size(&v) != tests[i].countNonEmpty) {
-            ret = TEST_ERR;
-        }
+        TEST_ASSERT(STRING_Split(s, &v, d, False) == tests[i].countNonEmpty);
+        TEST_ASSERT(VECTOR_Size(&v) == tests[i].countNonEmpty);
     }
     VECTOR_Destroy(&v);
-    return ret;
+    return TEST_OK;
 }
 
 int
